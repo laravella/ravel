c18788dcbec0b6c94071d5bde4875f6768f5f313
laravella <laravella.cms@gmail.com>
2013-07-11 13:57:43 +0200

    login active action navbar

--- src/views/layouts/admin/partials/navbar.blade.php
+++ src/views/layouts/admin/partials/navbar.blade.php
@@ -43,7 +43,7 @@
 					<li {{ (Request::is('account') ? 'class="active"' : '') }}><a href="{{ URL::to('account') }}">Account</a></li>
 					<li><a href="{{ URL::to('admin/logout') }}">Logout</a></li>
 					@else
-					<li {{ (Request::is('account/login') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/login/index') }}">Login</a></li>
+					<li {{ (Request::is('admin/login') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/login') }}">Login</a></li>
 					{{-- <li {{ (Request::is('account/register') ? 'class="active"' : '') }}><a href="{{ URL::to('account/register') }}">Register</a></li> --}}
 					@endif
 				</ul>
