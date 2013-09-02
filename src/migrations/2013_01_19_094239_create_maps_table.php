3581d73b979c9c13c1c9b75a6b2f334c38f8b2c4
laravella <laravella.cms@gmail.com>
2013-09-02 10:45:43 +0200

    fixed maps migration

--- src/migrations/2013_01_19_094239_create_maps_table.php
+++ src/migrations/2013_01_19_094239_create_maps_table.php
@@ -11,20 +11,23 @@ class CreateMapsTable extends Migration {
 	 * @return void
 	 */
 	public function up() {
-		Schema::create('maps', function(Blueprint $table) {
-					$table->increments('id');
-					$table->string('latitude', 7,5);
-					$table->string('longitude', 7,5);
-					$table->string('title');
-					$table->string('lang', 3)->nullable();
-					$table->integer('author_id')->unsigned()->nullable();
-					$table->string('status')->default('draft')->nullable(); //draft, submitted, published,
-					$table->datetime('publish_date')->nullable();
-					$table->datetime('deleted_at')->nullable();
-					$table->timestamps();
+            if (!Schema::hasTable('maps'))
+            {
+                    Schema::create('maps', function(Blueprint $table) {
+                                            $table->increments('id');
+                                            $table->string('latitude', 7,5);
+                                            $table->string('longitude', 7,5);
+                                            $table->string('title');
+                                            $table->string('lang', 3)->nullable();
+                                            $table->integer('author_id')->unsigned()->nullable();
+                                            $table->string('status')->default('draft')->nullable(); //draft, submitted, published,
+                                            $table->datetime('publish_date')->nullable();
+                                            $table->datetime('deleted_at')->nullable();
+                                            $table->timestamps();
 
-//					$table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
-				});
+    //					$table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+                                    });
+            }
 	}
 
 	/**
